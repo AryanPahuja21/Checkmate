8e0a9990e06ff816e6a71a33e4d894cd772bc6e5
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-07-24 01:36:38 +0530

    backend complete

--- backend/src/Game.ts
+++ backend/src/Game.ts
@@ -1,19 +1,85 @@
+import { Chess } from "chess.js";
 import { WebSocket } from "ws";
+import { INIT_GAME, MOVE } from "./messages";
 
 export class Game {
   public player1: WebSocket;
   public player2: WebSocket;
-  private board: string;
+  private board: Chess;
   private moves: string[];
   private startTime: Date;
 
   constructor(player1: WebSocket, player2: WebSocket) {
     this.player1 = player1;
     this.player2 = player2;
-    this.board = "";
+    this.board = new Chess();
     this.moves = [];
     this.startTime = new Date();
+    this.player1.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "white",
+        },
+      })
+    );
+    this.player2.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "black",
+        },
+      })
+    );
   }
 
-  makeMove(socket: WebSocket, move: string) {}
+  makeMove(
+    socket: WebSocket,
+    move: {
+      from: string;
+      to: string;
+    }
+  ) {
+    if (this.board.moves.length % 2 === 0 && socket !== this.player1) {
+      return;
+    }
+
+    if (this.board.moves.length % 2 === 1 && socket !== this.player2) {
+      return;
+    }
+
+    try {
+      this.board.move(move);
+    } catch (e) {
+      return;
+    }
+
+    if (this.board.isGameOver()) {
+      this.player1.emit(
+        JSON.stringify({
+          type: "game_over",
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+      return;
+    }
+
+    if (this.board.moves.length % 2 === 0) {
+      this.player2.emit(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    } else {
+      this.player1.emit(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    }
+  }
 }

84ac0c6dccf352ba14bcf1947d2a339a08f93a3e
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-07-24 18:35:49 +0530

    ui update

--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div className="h-screen bg-black/90 flex flex-col justify-between items-center">
+    <div className="h-screen bg-black/90 flex flex-col justify-between items-center select-none">
       <section className="flex flex-col justify-center items-center flex-grow lg:mt-24">
-        <h1 className="mb-7 text-4xl sm:text-6xl text-white font-bold">
+        <h1 className="mb-14 text-4xl sm:text-6xl text-white font-bold font-rowdies ">
           Checkmate
         </h1>
         <Link to="/game">
-          <button className="p-2 sm:px-4 sm:py-3 sm:text-xl text-white font-semibold tracking-wider bg-gradient-to-r from-green-600 to-emerald-800 rounded-lg">
+          <button className="p-2 sm:px-4 sm:py-3 sm:text-xl text-white font-semibold font-mono tracking-wide bg-gradient-to-r from-green-600 to-emerald-800 rounded-lg ">
             Play Online
           </button>
         </Link>
